f1f718c548a4993a9fb46b5844f71c9f890195bc
fffffatah <ab.fatahmn@gmail.com>
2023-03-06 18:34:01 +0600

    update app details

--- src/setPageTitle.js
+++ src/setPageTitle.js
@@ -1,20 +1,9 @@
 import { useEffect } from 'react';
 
 function SetPageTitle(title) {
-    //const defaultTitle = useRef(document.title);
-
     useEffect(() => {
         document.title = title;
     }, [title]);
-
-    // useEffect(
-    //     () => () => {
-    //         if (!prevailOnUnmount) {
-    //             document.title = defaultTitle.current;
-    //         }
-    //     },
-    //     []
-    // );
 }
 
 export default SetPageTitle;
